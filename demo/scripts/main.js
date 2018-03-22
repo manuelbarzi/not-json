b95a95ad359c863c2bfb225a9cdef9ad4674293d
manuelbarzi <manuelbarzi@gmail.com>
2018-03-22 21:34:19 +0100

    update

--- demo/scripts/main.js
+++
@@ -1,9 +0,0 @@
-var obj = HJSON.parse('{ a: 1, b: "2", c: \'3\', "d": [1, "2", \'3\'] }')
-
-console.log(obj)
-// { a: 1, b: '2', c: '3', d: [ 1, '2', '3' ] }
-
-var json = HJSON.stringify(obj)
-
-console.log(json)
-// {"a":1,"b":"2","c":"3","d":[1,"2","3"]}
---
+++ demos/module-demo/index.js
@@ -0,0 +1,11 @@
+const HJSON = require('hybrid-json')
+
+const hjson = '{ a: 1, b: "2", c: \'3\', "d": [1, "2", \'3\'] }'
+
+const obj = HJSON.parse(hjson)
+
+console.log(`Parsing string ${hjson} results in object`, obj) // { a: 1, b: '2', c: '3', d: [ 1, '2', '3' ] }
+
+const json = HJSON.stringify(obj)
+
+console.log(`Stringifying object { a: 1, b: '2', c: '3', d: [ 1, '2', '3' ] } results in string`, json) // {"a":1,"b":"2","c":"3","d":[1,"2","3"]}
